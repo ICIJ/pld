318e3581bc98bde8488285fa0c98329338e4f639
Pierre Romera <hello@pirhoo.com>
2023-06-15 00:31:52 +0200

    doc: add help texts

--- src/cli.py
+++ src/cli.py
@@ -7,10 +7,10 @@ from src.pld import PdfLanguageDetector
 app = typer.Typer()
 
 @app.command()
-def main(languages: List[str] = typer.Option(..., '--language'), 
-         input_dir: Path  = typer.Option(..., '--input_dir'),
-         output_dir: Optional[Path] = typer.Option('out'),
-         max_pages: Optional[int] = 5):
+def main(languages: List[str] = typer.Option(..., '--language', help="An ISO3 language code."), 
+         input_dir: Path  = typer.Option(..., '--input_dir', help="Path to the input directory."),
+         output_dir: Optional[Path] = typer.Option('out', help="Path to the output directory."),
+         max_pages: Optional[int] = typer.Option(5, help="Maximum number of pages to process per PDF file.")):
     """
     Process PDF files and detect the dominant language.
     """
